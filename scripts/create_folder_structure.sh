1eac441c91441f272725b015b961785868d20fc6
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-04-17 21:30:54 +0200

    remove old remnents and add some basic healthchecks

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -5,4 +5,4 @@ set +a
 
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
-mkdir -p "${DATA}"/{portainer,diun,ombi,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,bazarr,caddy_data/config}
+mkdir -p "${DATA}"/{portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,bazarr,caddy_data/config}
