b004af7cc0c910e4d958e1e780fd12867ab36f3e
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-04-24 22:02:36 +0200

    fix es issues

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -7,4 +7,6 @@ echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
 mkdir -p "${DATA}"/{elasticsearch,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,bazarr,caddy_data/config}
 
-sudo chown -R 1000:1000 "${DATA}/elasticsearch"
+sudo sysctl -w vm.max_map_count=262144
+sudo chmod g+rwx "${DATA}/elasticsearch"
+sudo chgrp 1000 "${DATA}/elasticsearch"
