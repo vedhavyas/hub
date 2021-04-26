0ff92dc480632cd259f9f55ea498dbc7d38bf8ee
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-04-27 00:35:51 +0200

    add dir permissions

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -5,4 +5,6 @@ set +a
 
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
-mkdir -p "${DATA}"/{filestash,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,bazarr,caddy_data/config}
+mkdir -p "${DATA}"/{filestash,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
+
+chown -R filestash:filestash "${DATA}/filestash"
