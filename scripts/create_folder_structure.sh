6902ec42778b319078f30679c8d728278598334a
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-04-27 01:01:25 +0200

    update filestash docker

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -6,5 +6,3 @@ set +a
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
 mkdir -p "${DATA}"/{filestash,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
-
-chown -R filestash:filestash "${DATA}/filestash"
