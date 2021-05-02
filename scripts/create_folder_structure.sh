aac5dfebfb7f707a3fadddc796365ff31ddc2fb1
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-05-02 15:01:40 +0200

    use filebrowser

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -5,4 +5,6 @@ set +a
 
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
-mkdir -p "${DATA}"/{filestash,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
+mkdir -p "${DATA}"/{filebrowser,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
+touch "$DATA"/filebrowser/filebrowser.db
+sudo chown -R "$PUID":"$PGID" "$DATA"/filebrowser
