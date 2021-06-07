ea8b7ab837039b05038cf830096d5432013012ea
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-06-07 15:26:10 +0200

    update permissions

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -5,6 +5,8 @@ set +a
 
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
-mkdir -p "${DATA}"/{filebrowser,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
+mkdir -p "${DATA}"/{postgres,planka,filebrowser,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
 touch "$DATA"/filebrowser/filebrowser.db
 sudo chown -R "$PUID":"$PGID" "$DATA"/filebrowser
+sudo chown -R "$PUID":"$PGID" "$DATA"/postgres
+sudo chown -R "$PUID":"$PGID" "$DATA"/planka
