f326ae3020ede32468deced786395aa145312d42
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-09-14 20:49:35 +0200

    fixes

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -6,7 +6,9 @@ set +a
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
 mkdir -p "${DATA}"/{postgres,planka,filebrowser,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
-touch "$DATA"/filebrowser/filebrowser.db
+if [ ! -e "$DATA"/filebrowser/filebrowser.db ]; then
+  touch "$DATA"/filebrowser/filebrowser.db
+fi
 sudo chown -R "$PUID":"$PGID" "$DATA"/filebrowser
 sudo chown -R "$PUID":"$PGID" "$DATA"/postgres
 sudo chown -R "$PUID":"$PGID" "$DATA"/planka
