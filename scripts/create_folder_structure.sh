7a833a6ac231a07f8845132f65f2dc7347287629
vedhavyas <vedhavyas.singareddi@gmail.com>
2021-11-29 13:33:45 +0100

    add calibre

--- scripts/create_folder_structure.sh
+++ scripts/create_folder_structure.sh
@@ -5,10 +5,11 @@ set +a
 
 echo "Main directory from .env ${DATA}"
 echo "Creating folder structure..."
-mkdir -p "${DATA}"/{postgres,planka,filebrowser,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
+mkdir -p "${DATA}"/{postgres,planka,calibre-web,filebrowser,redis,portainer,heimdall,bitwarden,jellyfin,media/{movies,tv,books/calibre,music,photos,videos,others,downloads},sonarr,radarr,jackett,caddy_data/config}
 if [ ! -e "$DATA"/filebrowser/filebrowser.db ]; then
   touch "$DATA"/filebrowser/filebrowser.db
 fi
 sudo chown -R "$PUID":"$PGID" "$DATA"/filebrowser
 sudo chown -R "$PUID":"$PGID" "$DATA"/postgres
 sudo chown -R "$PUID":"$PGID" "$DATA"/planka
+sudo chown -R "$PUID":"$PGID" "$DATA"/calibre-web
