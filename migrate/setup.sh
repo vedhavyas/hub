a5e0b09b6aefc6e6dbcdec0df7e317a880e98c96
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-02-06 18:55:30 +0100

    use docker group

--- migrate/setup.sh
+++ migrate/setup.sh
@@ -19,7 +19,7 @@ echo "docker installation done"
 
 # install docker-compose
 echo "installing docker compose..."
-curl -L "https://github.com/docker/compose/releases/download/2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 pip install docker-compose
