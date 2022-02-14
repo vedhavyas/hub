0d687897b38429a8ab6323ac8b1c10dfc7059a6a
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-02-14 20:48:36 +0100

    add sql scripts

---
+++ sql/add_pg_user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -u
+
+owner=${OWNER:-owner}
+database=${DATABASE:-database}
+docker exec -it postgres psql -U postgres -c \
+"CREATE USER $owner; CREATE DATABASE $database; GRANT ALL PRIVILEGES ON DATABASE $database TO $owner; FLUSH PRIVILEGES;"
