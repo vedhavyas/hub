48607118732376408393ee3852c39d5db87589ed
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-02-14 20:50:48 +0100

    remove flush

--- sql/add_pg_user.sh
+++ sql/add_pg_user.sh
@@ -6,4 +6,4 @@ set -u
 owner=${OWNER:-owner}
 database=${DATABASE:-database}
 docker exec -it postgres psql -U postgres -c \
-"CREATE USER $owner; CREATE DATABASE $database; GRANT ALL PRIVILEGES ON DATABASE $database TO $owner; FLUSH PRIVILEGES;"
+"CREATE USER $owner; CREATE DATABASE $database; GRANT ALL PRIVILEGES ON DATABASE $database TO $owner;"
