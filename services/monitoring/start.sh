3589aa3c0c0f9db983d8f73ec11d5f76778db91f
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-03 10:17:18 +0200

    fixes

--- services/monitoring/start.sh
+++ services/monitoring/start.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-echo "Starting maintenance services..."
-cd "${SRV_DIR}/maintenance"  || { echo "Maintenance services doesn't exist"; exit 1; }
+echo "Starting monitoring services..."
+cd "${SRV_DIR}/monitoring"  || { echo "Monitoring services doesn't exist"; exit 1; }
 docker compose up -d
 echo "Done."
