34a1c7d6ffbcf38529a01c697d3a14008eb15516
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-03 11:13:43 +0200

    maybe this works ?

--- services/docker/start.sh
+++ services/docker/start.sh
@@ -3,8 +3,6 @@
 echo "Setting up docker direct network..."
 docker stop "$(docker container ls -q)"
 docker system prune -a -f
-systemctl restart docker.socket
-systemctl restart docker
 docker network create --subnet 10.10.2.0/24 docker-direct &> /dev/null
 # forward packets from this network to any network
 ddif="br-${$(docker network inspect -f {{.Id}} docker-direct):0:12}"
