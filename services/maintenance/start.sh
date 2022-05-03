a3677490a7aae1b1d732cb5e84e18dd2f23658a3
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-03 10:34:21 +0200

    chown

--- services/maintenance/start.sh
+++ services/maintenance/start.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
 echo "Starting maintenance services..."
+mkdir -p "${DATA_DIR}"/portainer
+chown -R docker:docker "${DATA_DIR}"/portainer/*
 cd "${SRV_DIR}/maintenance"  || { echo "Maintenance services doesn't exist"; exit 1; }
 docker compose up -d
 echo "Done."
