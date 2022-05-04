214a904005c8517080e300191bd32f468260c7e2
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-04 11:04:00 +0200

    small fixes

--- services/media/start.sh
+++ services/media/start.sh
@@ -6,26 +6,30 @@ for i in prowlarr jackett media sonarr radarr emby ombi; do
   chown -R docker:docker "${DATA_DIR}"/${i}/*
 done
 
+source "${SRV_DIR}"/.env
+EXTERNAL_VPN=${EXTERNAL_VPN:-}
+export PEER_PORT=
+if [[ "${EXTERNAL_VPN}" != "" ]]; then
+  VPN_FORWARDED_PORT="${EXTERNAL_VPN:u}_VPN_FORWARDED_PORT"
+  export PEER_PORT=${(P)VPN_FORWARDED_PORT}
+fi
+
 cd "${SRV_DIR}/media"  || { echo "Media services doesn't exist"; exit 1; }
 docker compose up -d
 
-# port forward host to transmission
-source "${SRV_DIR}"/.env
-EXTERNAL_VPN=${EXTERNAL_VPN:-}
-if [[ "${EXTERNAL_VPN}" = "" ]]; then
-  exit 0
+if [ -z "${PEER_PORT}" ]; then
   echo "Done."
+  exit 0
 fi
 
 # wait for transmission to come up
 wait-for-it -t 60 10.10.3.100:9091
-VPN_FORWARDED_PORT="${EXTERNAL_VPN:u}_VPN_FORWARDED_PORT"
-wait-for-it -t 60 10.10.3.100:"${(P)VPN_FORWARDED_PORT}"
+wait-for-it -t 60 10.10.3.100:"${PEER_PORT}"
 
-# Accept any port forwards from the external vpn
+# port forward host to transmission
 inf=wg_${EXTERNAL_VPN}
 iptables -t nat -I PREROUTING -i "${inf}" -j MARK --set-mark 100
-iptables -t nat -A PREROUTING -i "${inf}" -p tcp --dport "${(P)VPN_FORWARDED_PORT}" -j DNAT --to 10.10.3.100:"${(P)VPN_FORWARDED_PORT}"
+iptables -t nat -A PREROUTING -i "${inf}" -p tcp --dport "${PEER_PORT}" -j DNAT --to 10.10.3.100:"${PEER_PORT}"
 
 echo "Done."
 
