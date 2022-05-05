234a54d787dd4814bbb34baadd23941535ab5d39
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-05 10:21:31 +0200

    media cleaner

--- builds/script.sh
+++ builds/script.sh
@@ -5,12 +5,12 @@ build)
   echo "Building docker images..."
   script_path=$(realpath "$0")
   dir=$(dirname "${script_path}")
-  for build in radicale webdav ; do
+  for build in radicale webdav media-cleaner; do
     docker build -t vedhavyas/${build}:latest "${dir}/${build}"
   done
   ;;
 push)
-  for build in radicale webdav ; do
+  for build in radicale webdav media-cleaner; do
     docker push vedhavyas/${build}:latest
   done
   ;;
--- sql/add_sql_user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-
-owner=${OWNER:-owner}
-database=${DATABASE:-database}
-docker exec -it mysql mysql -h 127.0.0.1 -P 3306 -u root -p -e \
-"CREATE USER $owner; CREATE DATABASE $database; GRANT ALL PRIVILEGES ON $database.* TO $owner; FLUSH PRIVILEGES;"
