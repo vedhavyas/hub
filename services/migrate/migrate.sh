513311a071a05f23bacb69f9b32328b8494eb160
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-05 19:00:29 +0200

    this works

--- services/migrate/migrate.sh
+++ services/migrate/migrate.sh
@@ -5,7 +5,7 @@ HUB_NEW_DATA_DIR=${HUB_NEW_DATA_DIR:-data}
 HUB_OLD_SSH=${HUB_OLD_SSH:-cloud}
 # this should resolve to public ip of the host
 # all need root password
-HUB_NEW_SSH=${HUB_NEW_SSH:-hub}
+HUB_NEW_SSH=${HUB_NEW_SSH:-"65.20.99.221"}
 
 case ${1} in
 "")
@@ -15,7 +15,7 @@ case ${1} in
   ;;
 *)
   echo "Syncing ${1} between machines... "
-  ssh -t admin@"$HUB_OLD_SSH" 'rsync -varlpogEtP /home/admin/my-cloud/'${HUB_OLD_DATA_DIR}/${1}/'  root@"'${HUB_NEW_SSH}'":/home/admin/hub/'${HUB_NEW_DATA_DIR}/${1}'' || exit 1
+  ssh -t root@"$HUB_OLD_SSH" 'rsync -varlpogEtP /home/admin/my-cloud/'${HUB_OLD_DATA_DIR}/${1}/'  root@"'${HUB_NEW_SSH}'":/home/admin/hub/'${HUB_NEW_DATA_DIR}/${1}'' || exit 1
   ;;
 esac
 
