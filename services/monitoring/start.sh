f8c37b2dfd1bc86d17f9335d1a05e63ef04927d0
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-08 23:16:41 +0200

    add flame

--- services/monitoring/start.sh
+++ services/monitoring/start.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 echo "Starting monitoring services..."
-mkdir -p "${DATA_DIR}"/grafana "${DATA_DIR}"/prometheus
+mkdir -p "${DATA_DIR}"/grafana "${DATA_DIR}"/prometheus "${DATA_DIR}"/flame
 chown docker:docker "${DATA_DIR}"/grafana
 chown docker:docker "${DATA_DIR}"/prometheus
+chown docker:docker "${DATA_DIR}"/flame
 cd "${SRV_DIR}/monitoring"  || { echo "Monitoring services doesn't exist"; exit 1; }
 docker compose up -d --quiet-pull --remove-orphans
 echo "Done."
