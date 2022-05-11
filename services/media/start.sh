fd4adefb4a2a26f1e561ff5e34eb1ee1f060f2ab
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-12 00:54:34 +0200

    add volume to script

--- services/media/start.sh
+++ services/media/start.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 echo "Starting media services..."
-for i in transmission prowlarr jackett media sonarr radarr emby ombi audiobookshelf; do
+for i in transmission prowlarr jackett media sonarr radarr emby ombi audiobookshelf podcasts; do
   mkdir -p "${DATA_DIR}"/${i}
   chown docker:docker "${DATA_DIR}"/${i}
 done
