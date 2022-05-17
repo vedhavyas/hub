bbe1257ea3442fd75fd8b7a0ab5d2537b2b01aaf
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-17 03:04:39 +0200

    samba volume adjustments

--- scripts/samba.sh
+++ scripts/samba.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 useradd -M "${SMB_USER}"
 (echo "${SMB_PASS}"; echo "${SMB_PASS}") | smbpasswd -L -D 3 -a -s "${SMB_USER}"
-COPY smb.conf /etc/samba/smb.conf
-mkdir -m 700 /time-machine/"${SMB_USER}"
-chown "${SMB_USER}" /time-machine/"${SMB_USER}"
+mkdir -m 700 /home/"${SMB_USER}"
+chown "${SMB_USER}" /home/"${SMB_USER}"
 /usr/sbin/smbd --daemon --foreground --no-process-group --log-stdout --debuglevel=3 --configfile=/etc/samba/smb.conf
