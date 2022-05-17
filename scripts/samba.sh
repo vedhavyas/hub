d07a5944511867fdfefa3336b2be652127df9b33
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-17 13:57:44 +0200

    some volume fixes

--- scripts/samba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install -y samba-libs
-useradd -M "${SMB_USER}"
-(echo "${SMB_PASS}"; echo "${SMB_PASS}") | smbpasswd -L -D 3 -a -s "${SMB_USER}"
-mkdir -m 700 /home/"${SMB_USER}"
-chown "${SMB_USER}" /home/"${SMB_USER}"
-/usr/sbin/smbd --daemon --foreground --no-process-group --log-stdout --debuglevel=3 --configfile=/etc/samba/smb.conf
