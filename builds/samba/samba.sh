3ba25a8ccb18de80d78090db49cb1a151935739e
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-17 22:47:32 +0200

    use another version

--- builds/samba/samba.sh
+++ builds/samba/samba.sh
@@ -5,4 +5,5 @@ usermod -aG "${SMB_USER}" "${SMB_USER}"
 (echo "${SMB_PASS}"; echo "${SMB_PASS}") | smbpasswd -L -D 3 -a -s "${SMB_USER}"
 mkdir -p /home/"${SMB_USER}"
 chown "${SMB_USER}":"${SMB_USER}" /home/"${SMB_USER}"
+chmod -R 700 "${SMB_USER}" /home/"${SMB_USER}"
 /usr/sbin/smbd --daemon --foreground --no-process-group --debuglevel=3 --debug-stdout --configfile=/etc/samba/smb.conf
