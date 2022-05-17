a4b285f4d385c1053b5df208f3692a50754d6569
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-17 19:43:54 +0200

    remove command arg for samba

--- builds/samba/samba.sh
+++ builds/samba/samba.sh
@@ -4,4 +4,4 @@ useradd -M "${SMB_USER}"
 (echo "${SMB_PASS}"; echo "${SMB_PASS}") | smbpasswd -L -D 3 -a -s "${SMB_USER}"
 mkdir -p /home/"${SMB_USER}"
 chown "${SMB_USER}" /home/"${SMB_USER}"
-/usr/sbin/smbd --daemon --foreground --no-process-group --log-stdout --debuglevel=3 --configfile=/etc/samba/smb.conf
+/usr/sbin/smbd --daemon --foreground --no-process-group --debuglevel=3 --configfile=/etc/samba/smb.conf
