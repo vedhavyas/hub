ceb92b60b59abb6f53809c53abd5006cf7a206e2
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-17 21:36:11 +0200

    more mount chnages

--- scripts/pre-mount.sh
+++ scripts/pre-mount.sh
@@ -9,8 +9,8 @@ rm -rf /hub
 # create required folders and setup ownership
 mkdir -p /hub
 mkdir -p /opt/rclone/logs /opt/rclone/cache
-chown -R docker:docker /hub
-chown -R docker:docker /opt/rclone
+chown docker:docker /hub
+chown docker:docker /opt/rclone
 
 # create conf
 cat > /opt/rclone/rclone.conf << EOF
@@ -26,6 +26,8 @@ remote = hub:hub
 password = ${RCLONE_CRYPT_PASSWORD}
 password2 = ${RCLONE_CRYPT_PASSWORD2}
 EOF
+
+chown docker:docker /opt/rclone/*
 
 # update fuse to allow others
 if ! (grep -iq "user_allow_other" /etc/fuse.conf && sed -i 's/.*user_allow_other.*/user_allow_other/' /etc/fuse.conf); then
