a6b173fdb03ba4ae42ca22a25a3c6dbbfd3847c4
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-17 20:12:14 +0200

    add group with user

--- builds/samba/samba.sh
+++ builds/samba/samba.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-useradd -M "${SMB_USER}" -g "${PGID}" -u "${PUID}"
+groupadd -g "$PGID" "$SMB_USER"
+useradd -M "$SMB_USER" -g "$SMB_USER" -u "${PUID}"
+usermod -aG "${SMB_USER}" "${SMB_USER}"
 (echo "${SMB_PASS}"; echo "${SMB_PASS}") | smbpasswd -L -D 3 -a -s "${SMB_USER}"
 mkdir -p /home/"${SMB_USER}"
-chown "${SMB_USER}" /home/"${SMB_USER}"
+chown "${SMB_USER}":"${SMB_USER}" /home/"${SMB_USER}"
 /usr/sbin/smbd --daemon --foreground --no-process-group --debuglevel=3 --debug-stdout --configfile=/etc/samba/smb.conf
