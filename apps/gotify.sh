9ad97e9b98c48b7872351de1b3dc560d88c63071
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-27 19:03:42 +0200

    remove extra eval

--- apps/gotify.sh
+++ apps/gotify.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -e
 GOTIFY_TITLE="$1"
-$GOTIFY_MESSAGE="$2"
+GOTIFY_MESSAGE="$2"
 test -z $GOTIFY_TOKEN && { echo "GOTIFY_TOKEN missing"; exit 1 }
 test -z $GOTIFY_TITLE && { echo "GOTIFY_TITLE missing"; exit 1 }
 test -z $GOTIFY_MESSAGE && { echo "GOTIFY_MESSAGE missing"; exit 1 }
