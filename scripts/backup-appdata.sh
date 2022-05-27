f89a641609319ce329560ec417578ec395e0baee
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-05-27 13:00:26 +0200

    differential backups

--- scripts/backup-appdata.sh
+++ scripts/backup-appdata.sh
@@ -1,25 +1,67 @@
 #!/bin/zsh
 set -e
 
+# we are doing a differential backup using tar
+# we always need the base.tgz. So we restore with this first
+# then we pick the diff-date.tgz to restore on top
+
+#test -z $SRC_DIR && { echo "source directory not set"; exit 1 }
+#test -z $BACKUP_DIR && { echo "backup directory not set"; exit 1 }
+
+SRC_DIR=$DATA_DIR
 BACKUP_DIR=$HUB_DIR/backups/hub/appdata
 
-# check if we have the backup before
-test -f "${BACKUP_DIR}"/last_backup.txt && last_backup_at=$(cat "${BACKUP_DIR}"/last_backup.txt) || touch "${BACKUP_DIR}"/last_backup.txt
+case $1 in
+backup )
+  # check if we have the backup before
+  test -f "${BACKUP_DIR}"/last_backup.txt && last_backup_at=$(cat "${BACKUP_DIR}"/last_backup.txt) || touch "${BACKUP_DIR}"/last_backup.txt
 
-# set current backup time
-current_backup_at=$(date +"%F-%I-%M%p")
+  # set current backup time
+  current_backup_at=$(date +"%F-%I-%M-%S%p")
 
-# is this a full sync
-if test -z "${last_backup_at}"; then
-  echo "Doing a full backup..."
-  tar -vv --create --gzip --listed-incremental="${BACKUP_DIR}"/base.sngz --file "${BACKUP_DIR}"/base.tgz "$DATA_DIR"
-else
-  echo "Doing a differential backup..."
-  cp "${BACKUP_DIR}"/base.sngz "${BACKUP_DIR}"/base-"${current_backup_at}".sngz
-  tar -vv --create --gzip --listed-incremental="${BACKUP_DIR}"/base-"${current_backup_at}".sngz --file "${BACKUP_DIR}"/diff-"${current_backup_at}".tgz "$DATA_DIR"
-fi
+  # is this a full sync
+  if test -z "${last_backup_at}"; then
+    echo "Doing a full backup..."
+    gtar -vv --create --one-file-system --gzip --listed-incremental="${BACKUP_DIR}"/base.sngz --file "${BACKUP_DIR}"/base.tgz "$SRC_DIR"
+  else
+    echo "Doing a differential backup..."
+    cp "${BACKUP_DIR}"/base.sngz "${BACKUP_DIR}"/base-"${current_backup_at}".sngz
+    gtar -vv --create --one-file-system --gzip --listed-incremental="${BACKUP_DIR}"/base-"${current_backup_at}".sngz --file "${BACKUP_DIR}"/diff-"${current_backup_at}".tgz "$SRC_DIR"
+  fi
 
-# update last backup time
-echo "${current_backup_at}" > "${BACKUP_DIR}"/last_backup.txt
+  # update last backup time
+  echo "${current_backup_at}" > "${BACKUP_DIR}"/last_backup.txt
+  echo "done."
+  ;;
 
-#TODO gotify message
+restore )
+  echo "Restoring data..."
+  # ensure directory is present
+  mkdir -p "$SRC_DIR"
+
+  # ensure directory is empty
+  test -z "$(ls -A "$SRC_DIR")" || { echo "Data directory must be empty"; exit 1 }
+
+  # do the base backup
+  # we use directory as / since we created with full path and tar tries to recreate it as is
+  gtar -vv --extract --file "${BACKUP_DIR}"/base.tgz --listed-incremental=/dev/null  --directory /
+
+  # get latest backup diff
+  test -f "${BACKUP_DIR}"/last_backup.txt && last_backup_at=$(cat "${BACKUP_DIR}"/last_backup.txt)
+  if test -z "${last_backup_at}"; then
+    echo "done."
+    exit 0
+  fi
+
+  gtar -vv --extract --file "${BACKUP_DIR}"/diff-"${last_backup_at}".tgz --listed-incremental=/dev/null  --directory /
+  echo "done."
+  ;;
+
+* )
+  echo "unknown command: ${1}"
+  exit 1
+  ;;
+esac
+
+
+
