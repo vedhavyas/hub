baace7397a76b1cd524fa7a2c70f822107d46d27
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-06-06 19:39:04 -0600

    remove subsapce node

--- scripts/services.sh
+++ scripts/services.sh
@@ -40,12 +40,7 @@ function entertainment_post_up() {
   wait-for-it -t 60 10.10.3.100:29850
 }
 
-function subspace_pre_up() {
-  mkdir -p /var/subspace/gemini-1b/archive-node
-  chown -R docker:docker /var/subspace/gemini-1b/archive-node/
-}
-
-services=(security comms maintenance monitoring entertainment utilities mailserver subspace)
+services=(security comms maintenance monitoring entertainment utilities mailserver)
 # start services
 for service in "${services[@]}"; do
   case $1 in
