47e5a1904de55e257b05ce927ad4d48d8a8797ba
vedhavyas <vedhavyas.singareddi@gmail.com>
2022-10-24 08:43:48 +0200

    build docker images on push and tweak archiver

--- scripts/extract_executables.sh
+++ scripts/extract_executables.sh
@@ -3,6 +3,7 @@ function extract_exec() {
     exec=$1
     dst=$2
     id=$(docker create vedhavyas/"${exec}":latest)
+    rm -rf "${dst}"
     docker cp "$id":/app/"${exec}" "${dst}"
     docker rm -v "$id"
 }
--- scripts/hub.sh
+++ scripts/hub.sh
@@ -44,15 +44,15 @@ certbot )
 
 backup)
   shift
-  data="${1:-appdata}"
-  run_script archiver backup "$DATA_DIR" $HUB_DIR/backups/hub/"${data}" || { hub notify "Hub updates" "Appdata backup failed!"; }
-  hub notify "Hub updates" "Appdata backup successful!"
+  data="${1:-hub}"
+  run_script archiver backup "$DATA_DIR" $HUB_DIR/backups/"${data}" || { hub notify "Hub updates" "Hub backup failed!"; }
+  hub notify "Hub updates" "Hub backup successful!"
   ;;
 
 restore)
   shift
-  data="${1:-appdata}"
-  run_script archiver restore "$DATA_DIR" $HUB_DIR/backups/hub/"${data}" || { hub notify "Hub updates" "Appdata restore failed!"; }
+  data="${1:-hub}"
+  run_script archiver restore "$DATA_DIR" $HUB_DIR/backups/"${data}" || { hub notify "Hub updates" "Appdata restore failed!"; }
   hub notify "Hub updates" "Appdata restore successful!"
   ;;
 
