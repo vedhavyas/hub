2c0e36cb002c5527195fa544e8fb8b1b631c491c
vedhavyas <vedhavyas.singareddi@gmail.com>
2024-04-13 22:52:13 +0530

    run media cleaner once a day

--- modules/media-cleaner/entrypoint.sh
+++ modules/media-cleaner/entrypoint.sh
@@ -7,7 +7,7 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 # Setup a cron schedule
 echo "SHELL=/bin/bash
 BASH_ENV=/container.env
-*/1 * * * * /media-cleaner > /proc/1/fd/1 2>/proc/1/fd/2
+0 0 * * * /media-cleaner > /proc/1/fd/1 2>/proc/1/fd/2
 # This extra line makes it a valid cron" > scheduler.txt
 
 crontab scheduler.txt
