c80c2c0869cd805be2e4cd6a8a7452fcdfa9a61c
vedhavyas <vedhavyas.singareddi@gmail.com>
2024-05-02 14:25:49 +0530

    update download cleaner to do recursive empty folder cleanup

--- modules/media-cleaner/download-cleaner.sh
+++ modules/media-cleaner/download-cleaner.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Running download-cleaner"
 
-echo "deleting files that are older than an hour"
+echo "deleting files that are older than a month"
 find /downloads -type f -mtime +30 -exec rm -rf {} \;
 
 echo "deleting the empty directories"
-find /downloads -mindepth 2 -type d ! -path '/downloads/games/.stfolder' -prune -type d -empty -exec rm -rf {} \;
+while [ "$(find /downloads -mindepth 2 -type d \( -path '/downloads/games/.stfolder' -prune \) -o -type d -empty -exec echo {} \; | wc -l)" -gt 0 ]
+do
+    find /downloads -mindepth 2 -type d \( -path '/downloads/games/.stfolder' -prune \) -o -type d -empty -exec rm -rf {} \;
+done
