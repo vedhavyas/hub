f0dc7f7a7f731eef8adf1a21509c0d16da1c5d9f
vedhavyas <vedhavyas.singareddi@gmail.com>
2025-02-11 21:27:38 +0530

    reduce download cleanup to 7 days from 30 days

--- modules/media-cleaner/download-cleaner.sh
+++ modules/media-cleaner/download-cleaner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running download-cleaner"
 
-echo "deleting files that are older than a month"
-find /downloads -type f -mtime +30 -exec rm -rf {} \;
+echo "deleting files that are older than 7 days"
+find /downloads -type f -mtime +7 -exec rm -rf {} \;
 
 echo "deleting the empty directories"
 while [ "$(find /downloads -mindepth 2 -type d \( -path '/downloads/games/.stfolder' -prune \) -o -type d -empty -exec echo {} \; | wc -l)" -gt 0 ]
